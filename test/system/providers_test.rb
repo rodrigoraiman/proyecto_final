4b70fef667fbc10f98c4ba6d958abe9ccb81603a
rodrigoraiman <rodrigoraiman@gmail.com>
2022-04-02 01:07:04 -0300

    iniciando proyecto con rails 7 and tailwind

---
+++ test/system/providers_test.rb
@@ -0,0 +1,47 @@
+require "application_system_test_case"
+
+class ProvidersTest < ApplicationSystemTestCase
+  setup do
+    @provider = providers(:one)
+  end
+
+  test "visiting the index" do
+    visit providers_url
+    assert_selector "h1", text: "Providers"
+  end
+
+  test "should create provider" do
+    visit providers_url
+    click_on "New provider"
+
+    fill_in "Company name", with: @provider.company_name
+    fill_in "Email", with: @provider.email
+    fill_in "Last name", with: @provider.last_name
+    fill_in "Name", with: @provider.name
+    click_on "Create Provider"
+
+    assert_text "Provider was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Provider" do
+    visit provider_url(@provider)
+    click_on "Edit this provider", match: :first
+
+    fill_in "Company name", with: @provider.company_name
+    fill_in "Email", with: @provider.email
+    fill_in "Last name", with: @provider.last_name
+    fill_in "Name", with: @provider.name
+    click_on "Update Provider"
+
+    assert_text "Provider was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Provider" do
+    visit provider_url(@provider)
+    click_on "Destroy this provider", match: :first
+
+    assert_text "Provider was successfully destroyed"
+  end
+end
